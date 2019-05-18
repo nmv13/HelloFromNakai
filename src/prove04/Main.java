c96d9c35e02c01a65d7c407bec8abe5b6e36292e
“Mario <das16005.byui.edu>
2019-05-18 15:35:38 -0600

    Mario Changed

--- src/prove04/Main.java
+++ src/prove04/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("Hello, my name is Nakai Velasquez");
+
+	    // its changed my friend
+        System.out.println(" Hello from Mario ");
+
     }
 }
